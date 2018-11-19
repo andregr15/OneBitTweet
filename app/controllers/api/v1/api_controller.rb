ed735244b6beb46c5d177f8586cd6ea5394d22f2
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-11-19 15:16:24 -0200

    Adding permissions rules to the project

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class ApiController < ApplicationController::API
       include Knock::Authenticable
+      include CanCan::ControllerAdditions
     end
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,5 +28,11 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+
+    can :read, Tweet
+    can :read, User
+    return unless user
+    can :manage, Tweet, user_id: user.id
+    can :manage, ser, id: user.id
   end
 end
