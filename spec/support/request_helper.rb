29878dabdcceda5c2045bd5115639954f09d62e2
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-11-20 15:16:40 -0200

    Adding request user (show and current methods) tests

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -22,7 +22,7 @@ module Requests
   module HeaderHelpers
     def header_with_authentication(user)
       token = Knock::AuthToken.new(payload: { sub: user.id }).token
-      return { 'Authorization' => "Bearer #{token}"}
+      return { 'Authorization' => "Bearer #{token}" }
     end
 
     def header_without_authentication
