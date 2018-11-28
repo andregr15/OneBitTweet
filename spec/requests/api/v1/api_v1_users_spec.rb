bec291246a27cfc4725f484317716adacbad1d01
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-11-28 10:36:40 -0200

    Adding delete, create and put tests for the user

--- spec/requests/api/v1/api_v1_users_spec.rb
+++ spec/requests/api/v1/api_v1_users_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'rspec/json_expectations'
 
 RSpec.describe "Api::V1::Users", type: :request do
   describe 'GET /api/v1/users/:id' do
@@ -81,4 +82,144 @@ RSpec.describe "Api::V1::Users", type: :request do
       end
     end 
   end
+
+  describe 'DELETE /api/v1/users/:id' do
+    context 'Unauthenticated' do
+      it_behaves_like :deny_without_authorization, :delete, '/api/v1/users/-1'
+    end
+
+    context 'Authenticated' do
+      context 'when user exists' do
+
+        context 'when user is the owner of the resource' do
+          before do 
+            @user = create(:user)
+            delete "/api/v1/users/#{@user.id}", headers: header_with_authentication(@user)
+          end
+
+          it 'should have returned no content http status' do
+            expect_status(:no_content)
+          end
+
+          it 'should have deleted the user' do
+            expect(User.count).to eq(0)
+          end
+        end
+
+        context 'when user is not the owner of the resource' do
+          before do
+            user = create(:user)
+            other_user = create(:user)
+
+            delete "/api/v1/users/#{other_user.id}", headers: header_with_authentication(@user)
+          end
+
+          it 'should have returned forbidden http status' do
+            expect_status(:forbidden)
+          end
+        end
+      end
+
+      context 'when user do not exists' do
+        before do
+          user = create(:user)
+          user_id = -1
+          delete "/api/v1/users/#{user_id}", headers: header_with_authentication(@user)
+        end
+
+        it 'should have returned not found http status' do
+          expect_status(:not_found)
+        end
+      end
+    end
+  end
+
+  describe 'POST /api/v1/users' do
+    context 'With valid params' do
+      before do
+        @user_params = attributes_for(:user)
+        post '/api/v1/users/', params: { user: @user_params }
+      end
+
+      it 'should have returned the http status created' do
+        expect_response(:created)
+      end
+
+      it 'should have returned the right user in json' do
+        expect(json).to include_json(user_params.except(:password))
+      end
+
+      it 'should have created the user' do
+        expect(User.count).to eql(1)
+      end
+
+    end
+
+    context 'With invalid params' do
+      before do
+        user_params = { foo: :bar}
+        post '/api/v1/users', params: { user: user_params }
+      end
+
+      it 'should have returned the http status unprocessable_entity' do
+        expect_response(:unprocessable_entity)
+      end
+    end
+
+  end
+
+  describe 'PUT /api/v1/users/:id' do
+    context 'Unauthenticated' do
+      it_behaves_like :deny_without_authorization, :put, '/api/v1/users/-1'
+    end
+
+    context 'Authenticated' do
+      context 'when user is the owner of the resource' do
+        context 'with valid params' do
+          before do
+            user = create(:user)
+            @user_params = attributes_for(:user)
+
+            put "/api/v1/users/#{user.id}", params: { user: @user_params }, headers: header_with_authentication(user)
+          end
+
+          it 'should have returned the http status success' do
+            expect_response(:success)
+          end
+
+          it 'should have returned the json with the user updated' do
+            expect(json).to include_json(@user_params.except(:password))
+          end
+        end
+
+        context 'with invalid params' do
+          before do
+            user = create(:user)
+            user_params = { foo: :bar }
+            put "/api/v1/users/#{user.id}", params: { user: user_params }, headers: header_with_authentication(user)
+          end
+
+          it 'should have returned the http status unprocessable entity' do
+            expect_status(:unprocessable_entity)
+          end
+
+        end
+      end
+
+      context 'when user is not the owner of the resource' do
+        before do
+          user = create(:user)
+          other_user = create(:user)
+          user_params = attributes_for(:user)
+
+          put "/api/v1/users/#{other_user.id}", params: { user: user_params }, headers: header_with_authentication(user)
+        end
+
+        it 'should have returnet the http status forbidden' do
+          expect_status(:forbidden)
+        end
+      end
+    end
+  end
+
 end
