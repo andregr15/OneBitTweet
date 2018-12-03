19d8e99f1ad3dc03fe1918627a47dc9b3af6587b
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-12-03 10:11:27 -0200

    Adding like tests

--- spec/requests/api/v1/api_v1_likes_spec.rb
+++ spec/requests/api/v1/api_v1_likes_spec.rb
@@ -1,10 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Likes", type: :request do
-  describe "GET /api_v1_likes" do
-    it "works! (now write some real specs)" do
-      get api_v1_likes_index_path
-      expect(response).to have_http_status(200)
+  describe "POST /api/v1/tweets/:id/like" do
+    context 'with invalid credentials' do
+      it_behaves_like :deny_without_authorization, :post, '/api/v1/tweets/-1/like'
+    end
+
+    context 'with valid credentials' do
+      before do
+        @user = create(:user)
+        @tweet = create(:tweet)
+        post "/api/v1/tweets/#{@tweet.id}/like", headers: header_with_authentication(@user)
+      end
+
+      it 'created the resource' do
+        expect(response).to have_http_status(:created)
+        expect(json['msg']).to eql('Liked with success')
+      end
+
+      it 'have the right tweet liked' do
+        expect(@tweet.get_likes.size).to eql(1)
+      end
+
+      it 'have the right user' do
+        expect(@tweet.get_likes[0].voter_id).to eql(@user.id)
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/tweets/:id/like' do
+    context 'with invalid credentials' do
+      it_behaves_like :deny_without_authorization, :delete, '/api/v1/tweets/-1/like'
+    end
+
+    context 'with valid credentials' do
+      before do
+        @user = create(:user)
+        @tweet = create(:tweet)
+        @user.likes @tweet
+        delete "/api/v1/tweets/#{@tweet.id}/like", headers: header_with_authentication(@user)
+      end
+
+      it 'destroy the resource' do
+        expect(response).to have_http_status(:no_content)
+      end
+
+      it 'have remove the right like' do
+        expect(@tweet.get_dislikes.size).to eql(1)
+      end
+
+      it 'have remove the right user' do
+        expect(@tweet.get_dislikes[0].voter_id).to eql(@user.id)
+      end
     end
   end
 end
