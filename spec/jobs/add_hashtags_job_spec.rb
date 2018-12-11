dff944c6b8d89229bb8b3d8c4e2058d601f4778f
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-12-11 09:26:26 -0200

    Adding update_trendings_job and add_hashtags_job tests

--- spec/jobs/add_hashtags_job_spec.rb
+++ spec/jobs/add_hashtags_job_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AddHashtagsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#perform_later' do
+    it 'updates trending tweets' do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        AddHashtagsJob.perform_later('teste')
+      }.to have_enqueued_job
+    end
+
+    it 'updates trending tweets in right queue' do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        AddHashtagsJob.perform_later('teste')
+      }.to have_enqueued_job.on_queue('trendings')
+    end
+  end
 end
