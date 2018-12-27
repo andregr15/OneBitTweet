c422e41f167ee824914891f44dabc9d0cb9190bd
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-12-27 14:31:31 -0200

    Adding description to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_165146) do
+ActiveRecord::Schema.define(version: 2018_12_27_163019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_165146) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.text "description"
   end
 
   create_table "votes", id: :serial, force: :cascade do |t|
